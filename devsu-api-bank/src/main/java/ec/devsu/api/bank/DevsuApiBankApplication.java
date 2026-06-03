0e54ebe04bf0ee5b57ab87f53ca4be68cd333a6e
EdisonR02 <riveraeddy58@gmail.com>
2026-06-02 21:15:22 -0500

    Initialize API and Web

---
+++ devsu-api-bank/src/main/java/ec/devsu/api/bank/DevsuApiBankApplication.java
@@ -0,0 +1,13 @@
+package ec.devsu.api.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsuApiBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsuApiBankApplication.class, args);
+	}
+
+}
---
+++ devsu-api-bank/src/test/java/ec/devsu/api/bank/DevsuApiBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.devsu.api.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsuApiBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
